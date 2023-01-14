4896e4991dd7c8f1f6f13af3c84cbcab64c8b7ab
yumcoder <yumcoder.platform@gmail.com>
2023-01-14 07:01:37 +0330

    add mutex lock problem when using in while let

---
+++ code/http13.rs
@@ -0,0 +1,15 @@
+// --snip--
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {id} got a job; executing.");
+
+                job();
+            }
+        });
+
+        Worker { id, thread }
+    }
+}
